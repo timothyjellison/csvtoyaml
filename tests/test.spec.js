59dbc365f0aff55f183b877dba764de801277b0e
Timothy Ellison <timothyellison85@gmail.com>
2018-04-17 20:55:57 -0400

    get test coverage to 100%

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -4,10 +4,21 @@ const path = require('path');
 const fs = require('fs');
 
 describe('csvtoyaml', () => {
-  it('outputs a Promise object', (done) => {
+  it('outputs a Promise object', () => {
     const yamlReader = csvtoyaml(`${__dirname}/test.csv`);
-
     expect(yamlReader).to.be.a('promise');
-    done();
+  });
+
+  it('reads data from a csv file and output yaml', async function() {
+    const data = await csvtoyaml(`${__dirname}/test.csv`);
+    expect(data).to.equal('---\n  - \n    a: "1"\n    b: "2"\n');
+  });
+
+  it('has a helpful error message if the user forgets to pass a filename', async function() {
+    try {
+      await csvtoyaml();
+    } catch(e) {
+      expect(e).to.equal('csvtoyaml requires a single argument: a string pointing to a csv file.');
+    }
   });
 });
