c6cf27949d111f3490e5f7e2bb988dde0c188966
Timothy Ellison <timothyellison85@gmail.com>
2018-06-23 15:17:07 -0400

    Cleanup code with eslint

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,7 +1,6 @@
+/* eslint no-undef: 0 */
+const { expect } = require('chai');
 const csvtoyaml = require('../index');
-const {expect} = require('chai');
-const path = require('path');
-const fs = require('fs');
 
 describe('csvtoyaml', () => {
   it('outputs a Promise object', () => {
@@ -9,16 +8,17 @@ describe('csvtoyaml', () => {
     expect(yamlReader).to.be.a('promise');
   });
 
-  it('reads data from a csv file and output yaml', async function() {
+  it('reads data from a csv file and output yaml', async () => {
     const data = await csvtoyaml(`${__dirname}/test.csv`);
     expect(data).to.equal('---\n  - \n    a: "1"\n    b: "2"\n');
   });
 
-  it('has a helpful error message if the user forgets to pass a filename', async function() {
+  it('throws an Error with helpful message if the user forgets to pass a filename', async () => {
     try {
       await csvtoyaml();
-    } catch(e) {
-      expect(e).to.equal('csvtoyaml requires a single argument: a string pointing to a csv file.');
+    } catch (e) {
+      expect(e.constructor === Error);
+      expect(e.message).to.equal('csvtoyaml requires a single argument: a string pointing to a csv file.');
     }
   });
 });
